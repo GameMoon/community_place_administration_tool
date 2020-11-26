9556d5f6ba92864e93a4c2e74f44760dffdad673
Daniel Ugrin <ugrindaniel@gmail.com>
2020-11-26 18:43:06 +0100

    websocket handling

--- frontend/src/Messages.js
+++ frontend/src/Messages.js
@@ -1,73 +1,87 @@
 import React from 'react'
 import Title from './Title'
 import { ChatList } from 'react-chat-elements'
-import { Box } from '@material-ui/core'
-import { Input } from 'react-chat-elements'
-import Button from '@material-ui/core/Button';
+import { Box, Button, TextField } from '@material-ui/core'
+import {WEBSOCKET_URL} from './config'
+import {useState,useEffect, useRef} from 'react'
+import { animateScroll } from "react-scroll";
 
 import 'react-chat-elements/dist/main.css';
 
 const Messages = ({ component: Component, ...rest }) => {
+    const [messages, setMessages] = useState([]);
+
+    const [newMessage, setNewMessage] = useState("");
+    const [isPaused, setPause] = useState(false);
+    const ws = useRef(null);
+    const chatContainerRef = useRef()
+
+    useEffect(() => {
+        ws.current = new WebSocket(WEBSOCKET_URL);
+        ws.current.onopen = () => console.log("ws opened");
+        ws.current.onclose = () => console.log("ws closed");
+
+        return () => {
+            ws.current.close();
+        };
+    }, []);
+
+    useEffect(() => {
+        if (!ws.current) return;
+
+        ws.current.onmessage = e => {
+            if (isPaused) return;
+            const message = JSON.parse(e.data);
+            setMessages(prevState => [...prevState , message])
+        };
+    }, [isPaused]);
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messages]);
+
+    const scrollToBottom = () => {
+        animateScroll.scrollToBottom({
+            containerId: "chat_container", smooth: false
+        });
+    }
+
+    const sendMessage = () => {
+        ws.current.send(JSON.stringify({message: newMessage}))
+        setNewMessage("")
+    };
+
+    const handleKeyDown = (e) => {
+        if(e.key === 'Enter') sendMessage()
+    }
 
     return (
         <>
         <Title>Messages</Title>
-        <Box textAlign="left" position="relative">
-            <Box style={{overflow: 'scroll'}} maxHeight="440px">
-            <ChatList
-                className='chat-list'
-                dataSource={[
-                    {
-                        title: 'Facebook',
-                        subtitle: 'What are you doing?',
-                        date: new Date(),
-                        unread: 0,
-                    },
-                    {
-                        title: 'Facebook',
-                        subtitle: 'What are you doing?',
-                        date: new Date(),
-                        unread: 0,
-                    },
-                    {
-                        title: 'Facebook',
-                        subtitle: 'What are you doing?',
-                        date: new Date(),
-                        unread: 0,
-                    },
-                    {
-                        title: 'Facebook',
-                        subtitle: 'What are you doing?',
-                        date: new Date(),
-                        unread: 0,
-                    },
-                    {
-                        title: 'Facebook',
-                        subtitle: 'What are you doing?',
-                        date: new Date(),
-                        unread: 0,
-                    }, {
-                        title: 'Facebook',
-                        subtitle: 'What are you doing?',
-                        date: new Date(),
-                        unread: 0,
-                    }, {
-                        title: 'Facebook',
-                        subtitle: 'What are you doing?',
-                        date: new Date(),
-                        unread: 0,
-                    }
-            ]} />
+        <Box textAlign="left" position="relative"style={{overflow: "hidden"}}>
+            <Box id="chat_container" style={{overflow: 'scroll', "overflow-x": 'hidden'}} height="370px" maxHeight="370px" >
+                <ChatList
+                    className='chat-list'
+                    dataSource={messages} />
             </Box>
+                <Box height="70px"></Box>
             <Box position='absolute' style={{bottom: 0, left: 0, right: 0}}>
-                <Input
-                    placeholder="Type here..."
-                    multiline={true}
-                    rightButtons={
-                        <Button size="small" color="primary" variant="contained" >
+                <Box display="flex" alignItems="center" bgcolor="white">
+                    <TextField
+                        id="filled-textarea"
+                        label="Say something"
+                        rows={2}
+                        fullWidth={true}
+                        onChange={(e) => { setNewMessage(e.target.value); }}
+                        onKeyDown={handleKeyDown}
+                        value={newMessage}
+                        multiline />
+                    <Box>
+                        <Button size="small" color="primary" variant="contained" onClick={sendMessage}>
                             Send
                         </Button>
-                    } />
+                    </Box>
+                </Box>
             </Box>
         </Box>
         </>
