37fcf5c1af8e18649f6edc0e44992eee7b6817d4
VoidX <voidxb@gmail.com>
2020-11-28 15:40:49 +0100

    Archive frontend

--- frontend/src/Archive.js
+++ frontend/src/Archive.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Grid, Box, Container } from '@material-ui/core/';
 import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import FolderIcon from '@material-ui/icons/Folder';
 import Header from './Header'
 import Copyright from './Copyright'
 import clsx from 'clsx'
-import {GDRIVE_FOLDER_ID} from './config'
+import { BACKEND_URL } from './config'
+import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import axios from 'axios';
+import moment from 'moment'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -35,31 +45,62 @@ const useStyles = makeStyles((theme) => ({
 const Archive = ({ component: Component, ...rest }) => {
     const classes = useStyles();
     const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+    
+    const [dates,setDates] = useState([]);
 
-    return (
-        <>
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(
+                BACKEND_URL + window.location.pathname,
+            );
+                
+            const newState = result.data.map(date => {
+                var newItem = Object.assign({}, date);
+                newItem.date = date.date;
+                newItem.time = date.time;
+                newItem.link = date.link;
+                return newItem;
+            });
+            setDates(newState);
+        };
+
+        fetchData();
+    }, []);
+
+    return (   
         <div className={classes.root}>
             <Header title="Archive" />
             <main className={classes.content}>
                 <div className={classes.appBarSpacer} />
                 <Container maxWidth="lg" className={classes.container}>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12}>
-                            {/* <Paper className={fixedHeightPaper}>
-                                <MyEvents />
-                            </Paper> */}
-                             <iframe title="gdrive_folder" src={"https://drive.google.com/embeddedfolderview?id="+GDRIVE_FOLDER_ID+"#list"} style={{width:"100%", height:"600px", border:"0"}}></iframe>
-                        </Grid>
-                        
-                    </Grid>
+                    <List>
+                      { dates.map((value) => {
+                        var link = "#";
+                        var target = "_self";
+                        if (value.date)
+                          link = "archive/" + value.date;
+                        else if (value.link) {
+                          link = "https://drive.google.com/file/d/" + value.link + "/view";
+                          target = "_blank"
+                        }
+                        return(
+                          <ListItem key={value} component="a" href={link} target={target}>
+                            <ListItemIcon>
+                              <FolderIcon />
+                            </ListItemIcon>
+                            <ListItemText
+                              primary={value.date ? value.date : value.time}
+                            />
+                          </ListItem>
+                        );
+                      })}
+                    </List>
                     <Box pt={4}>
                         <Copyright />
                     </Box>
                 </Container>
             </main>
         </div>
-        </>
-       
     )
 }
 
