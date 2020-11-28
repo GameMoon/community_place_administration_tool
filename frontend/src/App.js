18ac00bc3f97e464728e47860ea3a0dde2eb65a6
Daniel Ugrin <ugrindaniel@gmail.com>
2020-11-28 16:04:31 +0100

    Snackbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import SignIn from './SignIn'
+import SignUp from './SignUp'
 import Dashboard from './Dashboard'
 import PrivateRoute from './PrivateRoute'
 import EventManager from './EventManager'
 import Archive from './Archive';
+import SnackbarProvider from 'react-simple-snackbar'
 
 function App() {
   return (
+    <SnackbarProvider>
     <div className="App">
       <Switch>
         <Route exact path="/" component={SignIn}></Route>
+        <Route exact path="/signup" component={SignUp}></Route>
         <PrivateRoute path="/dashboard" component={Dashboard} /> 
         <PrivateRoute path="/events" component={EventManager} /> 
         <PrivateRoute path="/archive" component={Archive} /> 
         <Route>404 Page not found</Route>
       </Switch>
     </div>
+    </SnackbarProvider>
   );
 }
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,5 @@
 export const BACKEND_URL = "http://localhost:8000/api"
 export const WEBSOCKET_URL = "ws://127.0.0.1:8000/ws/chat/"
 export const GDRIVE_FOLDER_ID = "1daObWtgRz7Rhr-OkPFciW5XsOAalKrsL"
+export const SNACKBAR_ERROR_OPTIONS = {position: 'bottom-right', style: {backgroundColor: 'red', color: 'white'} }
+export const SNACKBAR_SUCCESS_OPTIONS = {position: 'bottom-right', style: {backgroundColor: 'green', color: 'white'} }
